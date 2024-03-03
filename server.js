70f796aab5a5d7062dd85b6355d399a70859c826
Sourav Sen <81104501+2Sourav@users.noreply.github.com>
2024-03-03 19:44:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ import authRoutes from "./routes/authRoute.js"
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path"
+import path from "path";
+import {fileURLToPath} from "url";
 //configure env
 dotenv.config();
 
 //connect Database
 connectDB();
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest object to create APIs
 const app=express()
